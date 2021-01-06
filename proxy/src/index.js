99fcf595c37002567340cf7c85a6a45ef9a7109e
Riikka Korolainen <riikka.korolainen@outlook.com>
2021-01-06 11:51:25 +0200

    proxy finished

---
+++ proxy/src/index.js
@@ -0,0 +1,26 @@
+const express = require('express')
+const request = require('request')
+
+const app = express()
+const API_URL = 'https://bad-api-assignment.reaktor.com/v2'
+
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*')
+  next()
+})
+
+app.get('/*', (req, res) => {
+  request(
+    { url: `${API_URL}${req.url}` },
+    (error, response, body) => {
+      if (error || response.statusCode !== 200) {
+        return res.status(500).end()
+      }
+
+      res.json(JSON.parse(body))
+    }
+  )
+})
+
+const PORT = process.env.PORT || 3001
+app.listen(PORT, () => console.log(`listening on ${PORT}`))
